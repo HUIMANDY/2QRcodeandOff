e599b78c7e524d159536f0606f0bd46d26716c35
HUIMANDY <80736252+HUIMANDY@users.noreply.github.com>
2021-04-13 21:20:21 +0800

    Add files via upload

--- Scanonoff.cs
+++ Scanonoff.cs
@@ -6,20 +6,38 @@ using MRTKExtensions.QRCodes;
 public class Scanonoff : MonoBehaviour
 {
     public GameObject QRcodescan;
+    //public GameObject qrcode;
+    public GameObject HandMenu;
+    public GameObject JetHolder;
+    public GameObject triggerObj;
+    public Canvas Canvas01;
     //public GameObject QRcodescan1;
     //public GameObject TRYT;
+    public bool QRonoff = false;
     public bool scann = true;
     public Text textscan;
+    public Text textt;
     // Start is called before the first frame update
     void Start()
     {
+        // qrcode = GameObject.Find("QRCODE");
+        HandMenu = GameObject.Find("HandMenu");
+        JetHolder = GameObject.Find("JetHolder");
+        triggerObj = GameObject.Find("triggerObj");
         QRcodescan = GameObject.Find("Trackers");
+        QRcodescan.SetActive(false);
+        HandMenu.SetActive(false);
+        JetHolder.SetActive(false);
+        //triggerObj.SetActive(false);
+        textt.text = "";
+        textscan.text = "";
+        //qrcode.SetActive(false);
     }
     public void scan()
 
     {
         textscan.text = "掃描開啟中";
-        
+
         //QRcodescan1 = GameObject.Find("Tracker2");
         //TRYT = GameObject.Find("JetHolder");
         if (scann == true)
@@ -41,10 +59,54 @@ public class Scanonoff : MonoBehaviour
             textscan.text = "掃描開啟中";
         }
     }
+    public void QRONOFF()
 
-    // Update is called once per frame
-    void Update()
     {
-        
+
+        if (QRonoff == true)
+        {
+            QRcodescan.SetActive(false);
+            HandMenu.SetActive(false);
+            JetHolder.SetActive(false);
+            //triggerObj.SetActive(false);
+            textt.text = "";
+            textscan.text = "";
+            QRonoff = false;
+            closeviss(QRloading.go1);
+        }
+        else
+        {
+
+            QRcodescan.SetActive(true);
+            HandMenu.SetActive(true);
+            JetHolder.SetActive(true);
+            //triggerObj.SetActive(true);
+            textt.text = "(0,0,0)";
+            textscan.text = "掃描開啟中";
+            QRonoff = true; ;
+
+        }
     }
+    public void closeviss(GameObject model)
+    {
+        foreach (Transform child in model.transform)
+        {
+            if (null == child)
+                continue;
+            if (child.gameObject.GetComponent<MeshRenderer>())
+            {
+                child.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            }
+
+            closeviss(child.gameObject);
+
+        }
+    }
+    
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    
 }
